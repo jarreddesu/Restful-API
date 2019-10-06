03aa213ac7baa3680e859fc1f3801b090a7f2578
jarreddesu <mikejarredevangelista@gmail.com>
2019-10-06 17:49:24 +0800

    Reviews Resource & Product Pagination

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -19,6 +19,7 @@ class ProductCollection extends Resource
             'totalPrice' => round((1-($this->discount/100)) * $this->price,2),
             'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count(), 2) : 'No rating yet',
             'discount' => $this->discount . '%',
+            'total reviews' => $this->reviews->count(),
             'href' => [
                 'link' => route('products.show', $this-> id)
             ]
